1c387c5df0576d30b4478fb7c98b3be4cb4c9310
mquoc313 <92698227+mquoc313@users.noreply.github.com>
2021-10-18 23:06:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,14 @@
 const CONFIG = {
     introTitle: 'hé lu',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    introDesc: `chào em `,
+    btnIntro: 'em có khỏe k đấy ',
     title: 'Phải chăng em đã mắc ỉa ',
-    desc: 'Phải chăng em đã ỉa đùn sau khi nghe anh nói  ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    desc: 'Phải chăng em đã ỉa đùn sau khi nghe anh nói áaaaaa ',
+    btnYes: 'dui ghê đĩ',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại mắc ỉa',
+    question:'mà sao em mắc địt quài dị đi ỉa đi thúi rùm trời rồi',
     btnReply: 'Gửi cho anh <3',
-    reply: 'ỉa thì ỉa mà không ỉa thì ỉa <33333333',
+    reply: 'do tui thích dị á chịu thì chịu hong chịu thì chịu',
     mess: 'Anh biết mà 🥰. ỉa đi trời đắc',
     messDesc: 'em mà hong ỉa anh lấy cái bô xi cho ỉa',
     btnAccept: 'Okiiiii lun <3',
